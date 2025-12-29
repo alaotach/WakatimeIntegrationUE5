27b191d9927039b506211c11c09574965155afdc
AlAoTach <81340648+alaotach@users.noreply.github.com>
2025-12-30 01:37:26 +0530

    Update WakatimeSettings.cpp

--- Source/WakatimeIntegration/Private/WakatimeSettings.cpp
+++ Source/WakatimeIntegration/Private/WakatimeSettings.cpp
@@ -3,6 +3,6 @@
 UWakatimeSettings::UWakatimeSettings()
 {
 	WakatimeBearerToken = TEXT("XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX");
-	WakatimeInterval = 60;
+	WakatimeInterval = 30;
 	WakatimeEndpoint = TEXT("https://wakatime.com/api/v1");
 }
